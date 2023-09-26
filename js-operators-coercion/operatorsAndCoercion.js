9a3e2a1f762da9490d06ecc6f9b0aae7fdea832f
somebodyakali <55654080+somebodyakali@users.noreply.github.com>
2023-09-26 14:51:44 +0100

    Added ; to the end of the lines

--- js-operators-coercion/operatorsAndCoercion.js
+++ js-operators-coercion/operatorsAndCoercion.js
@@ -28,16 +28,16 @@ let newVar = "5.489";
 // приведіть змінну до типу даних number, зі збереженням символів після коми
 
 let convertedNewVar = Number.parseFloat(newVar);
-console.log(convertedNewVar, typeof convertedNewVar)
+console.log(convertedNewVar, typeof convertedNewVar);
 
 // округліть значення до одного знака після крапки
 
-console.log(convertedNewVar.toFixed(1))
+console.log(convertedNewVar.toFixed(1));
 
 // поверніть змінну з типом даних number назад у тип даних string
 
 let convertedNewVar2 = convertedNewVar.toString();
-console.log(convertedNewVar2, typeof convertedNewVar2)
+console.log(convertedNewVar2, typeof convertedNewVar2);
 
 // 4.Створіть змінну зі значенням "Wikipedia is a free online encyclopedia, created and edited by volunteers around the world and hosted by the Wikipedia Foundation."
 
@@ -45,65 +45,65 @@ let wikiVar = "Wikipedia is a free online encyclopedia, created and edited by vo
 
 // виведіть довжину рядка
 
-console.log(wikiVar.length)
+console.log(wikiVar.length);
 
 // замініть всі слова Wikipedia на довільне слово чи букву, результат виведіть у консоль.
 
-let newWikiVar = wikiVar.replaceAll("Wikipedia", "Abc")
-console.log(newWikiVar)
+let newWikiVar = wikiVar.replaceAll("Wikipedia", "Abc");
+console.log(newWikiVar);
 
 // перевірте чи містить дана строка слово "world"
 
-let ifWorldIsInString = newWikiVar.toLowerCase().includes("world")
-console.log(ifWorldIsInString)
+let ifWorldIsInString = newWikiVar.toLowerCase().includes("world");
+console.log(ifWorldIsInString);
 
 // перевірте чи закінчується дана строка cловом "foundation"
 // Я так зрозумів, що очікуваний результат це true, тому зробив ще перевірку на слово foundation з крапкою, бо без крапки це false
 
-let ifWikiEndsWithFoundation_1 = newWikiVar.toLowerCase().endsWith("foundation")
-let ifWikiEndsWithFoundation_2 = newWikiVar.toLowerCase().endsWith("foundation.")
-console.log(ifWikiEndsWithFoundation_1)
-console.log(ifWikiEndsWithFoundation_2)
+let ifWikiEndsWithFoundation_1 = newWikiVar.toLowerCase().endsWith("foundation");
+let ifWikiEndsWithFoundation_2 = newWikiVar.toLowerCase().endsWith("foundation.");
+console.log(ifWikiEndsWithFoundation_1);
+console.log(ifWikiEndsWithFoundation_2);
 
 // 5* Обчислення площі та об'єму
 
 // Створіть змінну radius і присвойте їй числове значення радіуса кола.
 
-const radius = 5
+const radius = 5;
 
 // Обчисліть площу кола за формулою π * radius^2 і виведіть результат.
 
-let circleArea = Math.PI * radius**2
-console.log(circleArea)
+let circleArea = Math.PI * radius**2;
+console.log(circleArea);
 
 // Створіть змінну length і присвойте їй числове значення довжини прямокутника.
 
-let length = 5
+let length = 5;
 
 // Створіть змінну width і присвойте їй числове значення ширини прямокутника.
 
-let width = 10
+let width = 10;
 
 // Обчисліть площу прямокутника за формулою length * width і виведіть результат.
 
-let rectangleArea = length * width
-console.log(rectangleArea)
+let rectangleArea = length * width;
+console.log(rectangleArea);
 
 // Створіть змінну height і присвойте їй числове значення висоти циліндра.
 
-let height = 2
+let height = 2;
 
-let cylinderVolume = Math.PI * radius**2 * height
-console.log(cylinderVolume)
+let cylinderVolume = Math.PI * radius**2 * height;
+console.log(cylinderVolume);
 
 // Округліть кожне отримане значення до 2 значень після крапки
 
-mathValues = [circleArea, rectangleArea, cylinderVolume]
+mathValues = [circleArea, rectangleArea, cylinderVolume];
 
 for(i of mathValues){
     console.log(i.toFixed(2))
-}
+};
 
-// console.log(circleArea.toFixed(2))
-// console.log(rectangleArea.toFixed(2))
-// console.log(cylinderVolume.toFixed(2))
+// console.log(circleArea.toFixed(2));
+// console.log(rectangleArea.toFixed(2));
+// console.log(cylinderVolume.toFixed(2));
