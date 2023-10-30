3a66a533fb91e6ba8b03a8d99defab8ea18fa352
Bohdan Pryimak <bp@Bohdans-Air.broadband>
2023-10-30 23:42:48 +0000

    HW Cypress

--- cypress/e2e/my-test/firstTest.cy.js
+++ cypress/e2e/my-test/firstTest.cy.js
@@ -1,5 +1,15 @@
-describe("First test suite",()=>{
-    it("First test", ()=>{
-        
+describe("First test suite", ()=>{
+
+    context("First test suite - context", ()=>{
+
+    });
+
+    it("First test", () => {
+        cy.visit("localhost:4200/pages/")
+    });
+
+    describe("Second describe inside", ()=>{
+
     })
-})
+
+});
